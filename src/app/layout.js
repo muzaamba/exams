5e29c76a2128652d6e86a417db9805503d3de03e
soomarcode <Soomar1122!>
2026-05-13 00:32:52 +0300

    feat: integrate Matomo Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,21 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Matomo Analytics */}
+        <Script id="matomo-analytics" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://zeweno.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src='https://cdn.matomo.cloud/zeweno.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
       </head>
       <body className="min-h-screen antialiased">
         <ThemeProvider>
